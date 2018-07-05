1fa1525caab8a700a86899a3f07daccbcc77819d
Tomas Krchnak <tomas.krchnak@millennium.sk>
2018-07-05 19:20:10 +0200

    EF, migration enabled, seeding the database

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -4,5 +4,8 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool IsSuscribedToNewsletter { get; set; }
+        public MembershipType MembershipType { get; set; } //navigation property --> umoznuje navigaciu z jedneho typu do ineho
+        public byte MembershipTypeId { get; set; } //v niektorych pripadoch ked nechceme z DB ziskat aj previazane itemy hned, staci ziskat ID, treba zachovat nazvovu konvenciu aby EF dokazalo rozoznat, ze sa jedna o FK
     }
 }
