b2349a1b9a1a5daa10a3d3766555d51f003a1de6
Tomas Krchnak <tomas.krchnak@millennium.sk>
2018-07-07 15:19:39 +0200

    Section 4 - part 4

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -6,13 +6,19 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
+
         [Required]
         [StringLength(255)] //mozne pouzit Fluent API
         public string Name { get; set; }
+
         [Display(Name = "Day of Birth")] //umoznuje zmenu textu Labelu zobrazeneho pri kontrole
         public DateTime? Birthdate { get; set; }
+
         public bool IsSuscribedToNewsletter { get; set; }
+
         public MembershipType MembershipType { get; set; } //navigation property --> umoznuje navigaciu z jedneho typu do ineho
+
+        [Display(Name = "Membership type")]
         public byte MembershipTypeId { get; set; } //v niektorych pripadoch ked nechceme z DB ziskat aj previazane itemy hned, staci ziskat ID, treba zachovat nazvovu konvenciu aby EF dokazalo rozoznat, ze sa jedna o FK
     }
 }
---
+++ Vidly/ViewModels/NewCustomerViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Vidly.Models;
+
+namespace Vidly.ViewModels
+{
+    public class NewCustomerViewModel
+    {
+        public IEnumerable<MembershipType> MembershipTypes { get; set; } //pouzitie IEnumerable namiesto List zabezpecuje viac loosly coupled
+        public Customer Customer { get; set; }
+    }
+}
