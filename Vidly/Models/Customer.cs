670d53060bcdfef02747d9da185a143c9dafd227
Tomas Krchnak <tomas.krchnak@millennium.sk>
2018-07-07 11:19:32 +0200

    Section 3 _exercise 2

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Vidly.Models
 {
@@ -8,6 +9,7 @@ namespace Vidly.Models
         [Required]
         [StringLength(255)] //mozne pouzit Fluent API
         public string Name { get; set; }
+        public DateTime? Birthdate { get; set; }
         public bool IsSuscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; } //navigation property --> umoznuje navigaciu z jedneho typu do ineho
         public byte MembershipTypeId { get; set; } //v niektorych pripadoch ked nechceme z DB ziskat aj previazane itemy hned, staci ziskat ID, treba zachovat nazvovu konvenciu aby EF dokazalo rozoznat, ze sa jedna o FK
