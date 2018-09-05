0f32914ca23e9eb2dca6b281facba56802dd223d
Tomas Krchnak <tomas.krchnak@millennium.sk>
2018-09-05 14:21:00 +0200

    Uprava Movies pre DataTables + Delete funkcionalita

---
+++ Vidly/Controllers/Api/MoviesController.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using System.Web.Http;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class MoviesController : ApiController
+    {
+        ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        //DELETE /api/movies/id
+        [HttpDelete]
+        public IHttpActionResult DeleteMovie(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+            {
+                return BadRequest();
+            }
+
+            _context.Movies.Remove(movie);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
