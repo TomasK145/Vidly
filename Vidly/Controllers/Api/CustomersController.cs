0b8be3ee49d18fe045e9fda857931b48e1aa88e2
Tomas Krchnak <tomas.krchnak@millennium.sk>
2018-09-05 09:50:04 +0200

    Web API - build API controller

---
+++ Vidly/Controllers/Api/CustomersController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class CustomersController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        //GET /api/customers --> vstavana konvencia
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return _context.Customers.ToList();
+        }
+
+        //GET /api/customers/
+        public Customer GetCustomer(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound); //response ak nebol customer najdeny
+            }
+
+            return customer;
+        }
+
+        //POST /api/customers
+        //podla konvencie ak vytvarame resource je tento resource vrateny klientovi
+        [HttpPost] //action bude volana len pri POST requeste (alebo mozeme nazvat metodu PostCustomer (vtedy netreba uviest atribut) --> odporucane aplikovat atribut)
+        public Customer CreateCustomer(Customer customer)
+        {
+            if (!ModelState.IsValid) //overenie validity modelu vramci action
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            _context.Customers.Add(customer);
+            _context.SaveChanges(); //po ulozeni je nastavene ID, mozeme vratit klientovi
+
+            return customer;
+        }
+
+        //PUT /api/customers/
+        //podla konvencie moze byt vrateny void alebo objekt
+        [HttpPut]
+        public void UpdateCustomer(int id, Customer customer)
+        {
+            if (!ModelState.IsValid) //overenie validity modelu vramci action
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customerInDb == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            customerInDb.Name = customer.Name;
+            customerInDb.Birthdate = customer.Birthdate;
+            customerInDb.IsSuscribedToNewsletter = customer.IsSuscribedToNewsletter;
+            customerInDb.MembershipTypeId = customer.MembershipTypeId;
+
+            _context.SaveChanges();
+        }
+
+        //DELETE /api/customers/1
+        [HttpDelete]
+        public void DeleteCustomer(int id)
+        {
+            var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customerInDb == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            _context.Customers.Remove(customerInDb);
+            _context.SaveChanges();
+        }
+    }
+}
