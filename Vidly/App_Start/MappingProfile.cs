97568988727a92e10238fa776dccfca05ec6d6e1
Tomas Krchnak <tomas.krchnak@millennium.sk>
2018-09-05 13:34:57 +0200

    ziskanie hierarchickych data a ich zobrazenie (Include, Dto, definicia vramci DataTables)

--- Vidly/App_Start/MappingProfile.cs
+++ Vidly/App_Start/MappingProfile.cs
@@ -13,6 +13,9 @@ namespace Vidly.App_Start
         public MappingProfile()
         {
             Mapper.CreateMap<Customer, CustomerDto>();
+            //Mapper.CreateMap<Movie, MovieDto>();
+            Mapper.CreateMap<MembershipType, MembershipTypeDto>();
+
             Mapper.CreateMap<CustomerDto, Customer>().ForMember(c => c.Id, opt => opt.Ignore()); //ignorovanie pre Id property
             //automapper vyuziva refection pre ziskanie properties v objektoch a mapuje ich podla nazvov
             //Automapper je convention-based mapping tool
